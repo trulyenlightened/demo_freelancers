065fc84c7f41c4a58e570b33201b934a5cd2f923
Devansh Sanghvi <devansh@sanghviinfo.com>
2019-07-20 11:49:21 +0530

    test

--- App.js
+++ App.js
@@ -1,47 +1,20 @@
-import React,{Component} from 'react';
-import { StyleSheet, Text, View,Dimensions } from 'react-native';
-import { Scene, Router, ActionConst } from "react-native-router-flux";
-import Home from "./Home";
-import GetFrequency from './GetFrequency';
-import SetFrequency from './SetFrequency';
+import React, { Component } from "react";
+import { Scene, Router } from "react-native-router-flux";
+import test from "./test";
 
-let ScreenHeight = Dimensions.get("window").height;
-let ScreenWidth = Dimensions.get("window").width;
 class App extends Component {
-  render(){
-  return (
-    <Router hideNavBar='true'>
-    <Scene key="root">
-    <Scene
-      key="home"
-      component={Home}
-      initial={true}
-      title='Home'
-    />
-    <Scene
-      key="getFrequency"
-      component={GetFrequency}
-      title='Get Frequency'
-    />
-
-    <Scene
-      key="setFrequency"
-      component={SetFrequency}
-      title='Set Frequency'
-    />
-    </Scene>
-    </Router>
-  );
-}
+  render() {
+    return (
+      <Router hideNavBar="true">
+        <Scene key="root">
+          <Scene key="home" component={test} initial={true} title="Home" />
+        </Scene>
+      </Router>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
 
 export default App;
